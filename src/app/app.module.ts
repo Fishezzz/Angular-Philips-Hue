ecf857bf85834fe2814281a886b6465a057b86d0
Fishezzz <33294032+Fishezzz@users.noreply.github.com>
2019-12-13 00:31:50 +0100

    moved files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { ConfigService } from './services/config.service';
 import { AppComponent } from './app.component';
 import { ConfigComponent } from './components/config/config.component';
 import { SwitchComponent } from './components/switch/switch.component';
-import { LightsContainerComponent } from './lights-container/lights-container.component';
+import { LightsContainerComponent } from './components/lights-container/lights-container.component';
 
 @NgModule({
     declarations: [
